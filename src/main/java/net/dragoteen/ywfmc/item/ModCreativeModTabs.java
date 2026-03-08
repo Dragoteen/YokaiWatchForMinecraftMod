0f2b06db7e42619dc0a17c81ac3424a3e79e7263
Dragoteen <dragoteenofficiel@outlook.com>
2026-03-08 20:07:50 +0100

    Initial commit - Yo-kai Watch Minecraft Mod

---
+++ src/main/java/net/dragoteen/ywfmc/item/ModCreativeModTabs.java
@@ -0,0 +1,77 @@
+package net.dragoteen.ywfmc.item;
+
+import net.dragoteen.ywfmc.YoKaiWatchForMinecraftMod;
+import net.dragoteen.ywfmc.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, YoKaiWatchForMinecraftMod.MODID);
+
+    public static final RegistryObject<CreativeModeTab> YO_KAI_WATCH_FOR_MINECRAFT_MOD_TAB = CREATIVE_MODE_TABS.register("ywformc_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.YOKAI_WATCH.get()))
+                    .title(Component.translatable("creativetab.ywformc_tab"))
+                    .displayItems((itemDisplayParameters, output) -> {
+
+                        output.accept(ModItems.YOKAI_WATCH.get());
+                        output.accept(ModItems.YOKAI_WATCH_POCHE.get());
+                        output.accept(ModItems.YOKAI_WATCH_ZERO.get());
+                        output.accept(ModItems.YOKAI_WATCH_PROTOTYPE_U.get());
+                        output.accept(ModItems.YOKAI_WATCH_U1.get());
+                        output.accept(ModItems.YOKAI_WATCH_U2.get());
+                        output.accept(ModItems.YOKAI_WATCH_UE.get());
+                        output.accept(ModItems.YOKAI_WATCH_REVE.get());
+                        output.accept(ModItems.TREASURE_WATCH.get());
+                        output.accept(ModItems.CRYSTAL_TREASURE_WATCH.get());
+
+                        output.accept(ModItems.YOKAI_WATCH_ANCESTRALE.get());
+                        output.accept(ModItems.YOKAI_WATCH_ANCESTRALE_N.get());
+                        output.accept(ModItems.YOKAI_WATCH_ANCESTRALE_ZERO.get());
+                        output.accept(ModItems.YOKAI_WATCH_ANCESTRALE_GOD.get());
+                        output.accept(ModItems.YOKAI_WATCH_OGRE.get());
+                        output.accept(ModItems.YOKAI_WATCH_ANIMUS.get());
+
+                        output.accept(ModItems.BLASTER_YOKAI.get());
+                        output.accept(ModItems.FOREUSE_YOKAI.get());
+                        output.accept(ModItems.ASPIRATEUR_YOKAI.get());
+                        output.accept(ModItems.YOKAI_BAZOOKA.get());
+                        output.accept(ModItems.ENMA_BLADE.get());
+                        output.accept(ModItems.ENCHANTED_FLUTE.get());
+
+                        output.accept(ModItems.MEDALLIUM.get());
+                        output.accept(ModItems.YOVNIKAI_WATCH.get());
+                        output.accept(ModItems.CLE_YOKAI_WORLD.get());
+                        output.accept(ModItems.TELEMIR_POCHE.get());
+
+                        output.accept(ModItems.FRAGMENT_DE_YOKAI.get());
+                        output.accept(ModItems.VIEIL_ENGRENAGE.get());
+                        output.accept(ModItems.GRELOT_FELIN.get());
+                        output.accept(ModItems.MONTRE_DE_LUXE.get());
+                        output.accept(ModItems.BILLE_MYSTERIEUSE.get());
+
+                        output.accept(ModItems.PLAQUE_DAMULETTE.get());
+                        output.accept(ModItems.AMULETTE_FORME_DE_U.get());
+                        output.accept(ModItems.AMULETTE_COLOREE.get());
+
+                        output.accept(ModItems.U_UPDATE.get());
+                        output.accept(ModItems.Y9_PROCESSOR_CHIP.get());
+                        output.accept(ModItems.EXTREME_LAPIS.get());
+                        output.accept(ModItems.EXTREME_GOLD.get());
+
+
+                        output.accept(ModBlocks.BLOC_DE_MAGNETITE.get());
+                        output.accept(ModBlocks.WATCHMAKERS_WORKBENCH.get());
+                    })
+                    .build());
+
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
---
+++ src/main/java/net/dragoteen/ywfmc/item/ModItems.java
@@ -0,0 +1,102 @@
+package net.dragoteen.ywfmc.item;
+
+import net.dragoteen.ywfmc.YoKaiWatchForMinecraftMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, YoKaiWatchForMinecraftMod.MODID);
+
+
+    public static final RegistryObject<Item> YOKAI_WATCH = ITEMS.register("yokai_watch",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> YOKAI_WATCH_POCHE = ITEMS.register("yokai_watch_poche",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> YOKAI_WATCH_ZERO = ITEMS.register("yokai_watch_zero",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> YOKAI_WATCH_PROTOTYPE_U = ITEMS.register("yokai_watch_prototype_u",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> YOKAI_WATCH_U1 = ITEMS.register("yokai_watch_u1",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> YOKAI_WATCH_U2 = ITEMS.register("yokai_watch_u2",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> YOKAI_WATCH_UE = ITEMS.register("yokai_watch_ue",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> YOKAI_WATCH_REVE = ITEMS.register("yokai_watch_reve",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> TREASURE_WATCH = ITEMS.register("treasure_watch",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CRYSTAL_TREASURE_WATCH = ITEMS.register("crystal_treasure_watch",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> YOKAI_WATCH_ANCESTRALE = ITEMS.register("yokai_watch_ancestrale",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> YOKAI_WATCH_ANCESTRALE_N = ITEMS.register("yokai_watch_ancestrale_n",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> YOKAI_WATCH_ANCESTRALE_ZERO = ITEMS.register("yokai_watch_ancestrale_zero",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> YOKAI_WATCH_ANCESTRALE_GOD = ITEMS.register("yokai_watch_ancestrale_god",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> YOKAI_WATCH_OGRE = ITEMS.register("yokai_watch_ogre",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> YOKAI_WATCH_ANIMUS = ITEMS.register("yokai_watch_animus",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> BLASTER_YOKAI = ITEMS.register("blaster_yokai",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FOREUSE_YOKAI = ITEMS.register("foreuse_yokai",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ASPIRATEUR_YOKAI = ITEMS.register("aspirateur_yokai",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> YOKAI_BAZOOKA = ITEMS.register("yokai_bazooka",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ENMA_BLADE = ITEMS.register("enma_blade",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ENCHANTED_FLUTE = ITEMS.register("enchanted_flute",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> FRAGMENT_DE_YOKAI = ITEMS.register("fragment_de_yokai",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> VIEIL_ENGRENAGE = ITEMS.register("vieil_engrenage",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> GRELOT_FELIN = ITEMS.register("grelot_felin",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> MONTRE_DE_LUXE = ITEMS.register("montre_de_luxe",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> BILLE_MYSTERIEUSE = ITEMS.register("bille_mysterieuse",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PLAQUE_DAMULETTE = ITEMS.register("plaque_damulette",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> AMULETTE_FORME_DE_U = ITEMS.register("amulette_forme_de_u",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> AMULETTE_COLOREE = ITEMS.register("amulette_coloree",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> U_UPDATE = ITEMS.register("u_update",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> Y9_PROCESSOR_CHIP = ITEMS.register("y9_processor_chip",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> EXTREME_LAPIS = ITEMS.register("extreme_lapis",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> EXTREME_GOLD = ITEMS.register("extreme_gold",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> MEDALLIUM = ITEMS.register("medallium",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> YOVNIKAI_WATCH = ITEMS.register("yovnikai_watch",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> CLE_YOKAI_WORLD = ITEMS.register("cle_yokai_world",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> TELEMIR_POCHE = ITEMS.register("telemir_poche",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
